12cbbc3d5caf816c6dd4daeba14baf27757d92fe
william <william@williams-MacBook-Pro.local>
2017-10-24 22:26:07 +1100

    landing page

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+    <h1>
+    Owneroom
+    </h1>
+    Own. Why rent?
+    </div>
+  );
+};
+
+export default Landing;
